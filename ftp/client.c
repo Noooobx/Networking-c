30e7978dfa84f7cfc536ba9c2e1025eccee43106
Nandakishor A.S <112534151+Noooobx@users.noreply.github.com>
2025-03-06 14:54:47 +0530

    Update ftp client

--- ftp/client.c
+++ ftp/client.c
@@ -1,46 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
-#define port 4330
+// Connect to FTP server on port 21
+int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+struct sockaddr_in server;
+server.sin_family = AF_INET;
+server.sin_port = htons(21); // FTP uses port 21 by default
+server.sin_addr.s_addr = inet_addr("server_ip"); // replace with actual FTP server IP
+int con_status = connect(sockfd, (struct sockaddr*)&server, sizeof(server));
 
-int main(){
-
-    // TCP socket.
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in server;
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(port);
-    server.sin_family = AF_INET;
-    int len = sizeof(server);
-
-    int con_status = connect(sockfd, (struct sockaddr*)&server, len);
-
-    if(con_status<0){
-        perror("coneenciton failed");
-        exit(0);
-    }
-
-    char filename[512];
-
-    // Sets the enitire array to 0.
-    bzero(filename,512);
-    printf("Enter the file name :");
-
-    scanf("%s",filename);
-
-    // Send filename and lenght of the array to the server.
-    send(sockfd, filename, strlen(filename), 0);
-
-    char output[2048];
-    // Sets the enitire array to 0.
-    bzero(output, 2048);
-
-    recv(sockfd, output, 2048, 0);
-    printf("\n\n------File Content------\n\n%s\n\n------File Content------\n\n",output);
-    close(sockfd);
-
-    return 0;
+if (con_status < 0) {
+    perror("Connection failed");
+    exit(0);
 }
+
+// Send FTP commands (e.g., USER, PASS) to authenticate
+send(sockfd, "USER username\r\n", strlen("USER username\r\n"), 0);
+send(sockfd, "PASS password\r\n", strlen("PASS password\r\n"), 0);
+
+// Request a file using RETR command
+send(sockfd, "RETR filename\r\n", strlen("RETR filename\r\n"), 0);
+
+// Receive the file content
+char buffer[1024];
+int bytes_received;
+while ((bytes_received = recv(sockfd, buffer, sizeof(buffer), 0)) > 0) {
+    // Process the received file content (e.g., save to disk)
+}
+
+close(sockfd);
