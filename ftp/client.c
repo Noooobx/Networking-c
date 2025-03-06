8480c4517aed3bcdc36e45015cc485832754e954
Nandakishor A.S <112534151+Noooobx@users.noreply.github.com>
2025-03-06 15:29:41 +0530

    Update client.c

--- ftp/client.c
+++ ftp/client.c
@@ -1,28 +1,82 @@
-// Connect to FTP server on port 21
-int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-struct sockaddr_in server;
-server.sin_family = AF_INET;
-server.sin_port = htons(21); // FTP uses port 21 by default
-server.sin_addr.s_addr = inet_addr("server_ip"); // replace with actual FTP server IP
-int con_status = connect(sockfd, (struct sockaddr*)&server, sizeof(server));
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-if (con_status < 0) {
-    perror("Connection failed");
-    exit(0);
+#define MAX_BUFFER 1024
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <server_ip> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    // Get server IP and port from command line arguments
+    char *server_ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        exit(1);
+    }
+
+    // Set up server address
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = inet_addr(server_ip);
+
+    // Connect to FTP server
+    if (connect(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        perror("Connection failed");
+        close(sockfd);
+        exit(1);
+    }
+
+    char command[MAX_BUFFER];
+    char filename[MAX_BUFFER];
+    char buffer[MAX_BUFFER];
+    int bytes_received;
+
+    // Get user input for commands
+    while (1) {
+        printf("Enter command: ");
+        scanf("%s", command);
+
+        if (strcmp(command, "get") == 0) {
+            printf("Enter filename: ");
+            scanf("%s", filename);
+
+            // Send 'get' command to the server
+            snprintf(buffer, sizeof(buffer), "get %s", filename);
+            send(sockfd, buffer, strlen(buffer), 0);
+
+            // Receive the file or error message from the server
+            while ((bytes_received = recv(sockfd, buffer, sizeof(buffer), 0)) > 0) {
+                // Write received data to a local file
+                FILE *file = fopen(filename, "wb");
+                if (file != NULL) {
+                    fwrite(buffer, 1, bytes_received, file);
+                    fclose(file);
+                    printf("FILE %s RECEIVED FROM SERVER\n", filename);
+                } else {
+                    printf("Error opening file to write\n");
+                }
+            }
+        }
+        else if (strcmp(command, "close") == 0) {
+            // Close the connection
+            close(sockfd);
+            printf("Connection closed\n");
+            break;
+        }
+        else {
+            printf("Invalid command!\n");
+        }
+    }
+
+    return 0;
 }
-
-// Send FTP commands (e.g., USER, PASS) to authenticate
-send(sockfd, "USER username\r\n", strlen("USER username\r\n"), 0);
-send(sockfd, "PASS password\r\n", strlen("PASS password\r\n"), 0);
-
-// Request a file using RETR command
-send(sockfd, "RETR filename\r\n", strlen("RETR filename\r\n"), 0);
-
-// Receive the file content
-char buffer[1024];
-int bytes_received;
-while ((bytes_received = recv(sockfd, buffer, sizeof(buffer), 0)) > 0) {
-    // Process the received file content (e.g., save to disk)
-}
-
-close(sockfd);
